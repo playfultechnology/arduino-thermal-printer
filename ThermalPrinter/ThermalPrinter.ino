69f7a5173dfc493379e062b43d16ba29818071eb
alastaira <alastair@playful.technology>
2019-09-19 16:15:12 +0100

    Initial Commit
    Added Arduino code and Fritzing project

---
+++ ThermalPrinter/ThermalPrinter.ino
@@ -0,0 +1,28 @@
+// INCLUDES
+// Create a serial connection between Arudino and printer
+#include <SoftwareSerial.h>
+// Modified from https://github.com/bildr-org/Thermal-Printer
+#include "src/Thermal/Thermal.h"
+// Bitmap image data
+#include "image.h"
+
+// GLOBALS
+// Initialise printer object with specified Rx, Tx pins and Baud Rate
+// Printer Tx -> Arduino Rx = Blue wire to Pin 2
+// Arduino Tx -> Printer Rx = Green wire to Pin 3
+Thermal printer(2, 3, 9600); 
+
+void setup(){
+  // Set font size [S,M,L]
+  printer.setSize('S');
+  // Print the specified line of text 
+  printer.println("STOP FIDDLING WITH IT, DOUGIE!"); //print line
+
+  // Print the image defined in image.h:
+  printer.printBitmap(image_width, image_height, image_data);
+}
+
+void loop(){
+ 
+ 
+}
