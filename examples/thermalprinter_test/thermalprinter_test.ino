87a47f27aa19dd9f25ab665a41b543cb931e9a91
alastaira <alastair@playful.technology>
2019-10-12 12:00:11 +0100

    Update to match Arduino folder structure

--- examples/thermalprinter_test/thermalprinter_test.ino
+++ examples/thermalprinter_test/thermalprinter_test.ino
@@ -1,8 +1,8 @@
 // INCLUDES
 // Create a serial connection between Arduino and printer
 #include <SoftwareSerial.h>
-// Thermal printer library modified from https://github.com/bildr-org/Thermal-Printer
-#include "src/Thermal/Thermal.h"
+// Assume Thermal Printer library has been installed into global Arduino libraries folder
+#include <ThermalPrinter.h>
 // Bitmap image data
 #include "image.h"
 
@@ -10,7 +10,7 @@
 // Initialise printer object with specified Rx, Tx pins and Baud Rate
 // Printer Tx -> Arduino Rx = Blue wire to Pin 2
 // Arduino Tx -> Printer Rx = Green wire to Pin 3
-Thermal printer(2, 3, 9600); 
+PlayfulTechnology::ThermalPrinter printer(2, 3, 9600); 
 
 void setup(){
   // Set font size [S,M,L]
